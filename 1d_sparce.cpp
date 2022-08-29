da9bcc7471742e92d8566944e1b7512ae34578d2
HusseinHany <hhany3997@gmail.com>
2022-08-29 13:55:25 +0200

    write a.l.l. constructor

--- 1d_sparce.cpp
+++ 1d_sparce.cpp
@@ -32,6 +32,11 @@ public:
     // default constructor
     ArrayLinkedList() : head(nullptr), tail(nullptr) {}
 
+    ArrayLinkedList(int length) {
+        for (int i = 0; i < length; ++i)
+            set_value(0, i);
+    }
+
     void set_value(int val, int idx) {
         Node* item = new Node(val, idx);
 
@@ -90,7 +95,7 @@ public:
 };
 
 int main() {
-    ArrayLinkedList list;
+    ArrayLinkedList list(10);
     list.set_value(50, 5);
     list.set_value(10, 1);
     list.set_value(60, 6);
