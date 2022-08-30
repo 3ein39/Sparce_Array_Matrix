ca1c76e2fde02a590ddfa0b6611b5afaf7bcbd46
HusseinHany <hhany3997@gmail.com>
2022-08-30 19:53:20 +0200

    test sparse matrix

--- 2d_sparce.cpp
+++ 2d_sparce.cpp
@@ -238,11 +238,30 @@ public:
 };
 
 
+void test_sparse() {
+    SparseMatrix mat(10, 10);
+    mat.set_value(5, 3, 5);
+    mat.set_value(7, 3, 7);
+    mat.set_value(2, 3, 2);
+    mat.set_value(0, 3, 2);
+    mat.set_value(6, 5, 6);
+    mat.set_value(4, 5, 4);
+    mat.set_value(3, 7, 3);
+    mat.set_value(1, 7, 1);
+    //mat.set_value(1, 70, 1);
+    mat.print_matrix();
+    mat.print_matrix_nonzero();
+
+    SparseMatrix mat2(10, 10);
+    mat2.set_value(5, 1, 9);
+    mat2.set_value(6, 3, 8);
+    mat2.set_value(9, 9, 9);
+    mat.add(mat2);
+    mat.print_matrix_nonzero();
+}
 
 int main() {
-    SparseMatrix mat(10, 10);
-
-    mat.print_matrix();
+    test_sparse();
 
     cout << "\n\n\nNO RTE\n";
 
